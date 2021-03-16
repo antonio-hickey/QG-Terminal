3cfe75cbca2928e45b0636e9b8fe1ca7eafa7445
Antonio Hickey <antoniohickey99@gmail.com>
2021-03-16 18:10:03 -0400

    Routes & Server

---
+++ index.py
@@ -0,0 +1,38 @@
+#------------------------------------
+# Import Modules                    #
+#------------------------------------
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+from app import app
+from apps import home, esChain, usRates, portfolio
+#------------------------------------
+# Layout                            #
+#------------------------------------
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
+])
+#------------------------------------
+# Callbacks                         #
+#------------------------------------
+@app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/apps/home':
+        return home.layout
+    elif pathname == '/apps/esChain':
+        return esChain.layout
+    elif pathname == '/apps/usRates':
+        return usRates.layout
+    elif pathname == '/apps/portfolio':
+        return portfolio.layout
+    else:
+        return '404'
+#------------------------------------
+#------------------------------------
+# Running Server                    #
+#------------------------------------
+if __name__ == '__main__':
+    app.run_server(debug=True)
+#------------------------------------
